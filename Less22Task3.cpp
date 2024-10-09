9ede4f268e042c707126b4e47ac7380e7caf406e
Havingworld <84483386+Havingworld@users.noreply.github.com>
2024-10-09 11:58:57 +1000

    Add files via upload

---
+++ Less22Task3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+
+bool areAnagrams(std::string& str1, std::string& str2) {
+    // Проверяем, что длины строк равны
+    if (str1.length() != str2.length()) {
+        return false;
+    }
+    // Создаем частотные карты символов для каждой строки
+    std::map<char, int> freq1, freq2;
+    for (char c : str1) {
+        freq1[c]++;
+    }
+    for (char c : str2) {
+        freq2[c]++;
+    }
+
+    // Сравниваем частотные карты
+    return freq1 == freq2;
+}
+
+int main() {
+    std::string str1{ "" }, str2{ "" };
+    
+    std::cout << "enter first word: ";
+    std::cin >> str1;
+    std::cout << std::endl << "enter second word: ";
+    std::cin >> str2;
+
+    if (areAnagrams(str1, str2)) {
+        std::cout << "The strings are anagrams" << std::endl;
+    }
+    else {
+        std::cout << "The strings are not anagrams" << std::endl;
+    }
+
+    return 0;
+}
